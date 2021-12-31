4ae3f75f88adcd4bc3860f26df6a168624045077
“Amrit <amritsingh@Amrits-MacBook-Pro.local>
2021-12-31 19:59:34 +0530

    Passing props to Todo component

--- src/App.js
+++ src/App.js
@@ -4,7 +4,11 @@ function App() {
   return (
     <div>
       <h1>My Todos</h1>
-      <Todo />
+      <Todo text="Learn React"/>
+      <Todo text="Learn React Hooks"/>
+      <Todo text="Master React"/>
+      <Todo text="Learn Next"/>
+      <Todo text="Learn Prisma"/>
     </div>
   );
 }
