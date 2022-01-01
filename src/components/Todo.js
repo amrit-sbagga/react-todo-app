8da8aa97c206ca903bc53289477184ac301d1931
“Amrit <amritsingh@Amrits-MacBook-Pro.local>
2022-01-01 20:03:20 +0530

    Working with event props

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -11,15 +11,19 @@ function Todo(props) {
     setModalIsOpen(true);
   }
 
+  function closeModalHandler(){
+    setModalIsOpen(false);
+  }
+
   return (
     <div className="card">
       <h2>{props.text}</h2>
       <div className="actions">
         <button className="btn" onClick = {deleteHandler}>Delete</button>
       </div>
-      { modalIsOpen && <Modal /> }
-      { modalIsOpen && <Backdrop /> }
-      
+      { modalIsOpen && <Modal onCancel={closeModalHandler} onCancel={closeModalHandler}/> }
+      { /* onCancel is prop name here for our custom component i.e Backdrop */}
+      { modalIsOpen && <Backdrop onCancel={closeModalHandler}/> }      
     </div>
   );
 }
